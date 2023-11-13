e71b92280763058eb45fc65232cc0bcd7816bc45
SaraTiburcioEsteban <zs20016130@estidiantes.uv.mx>
2023-11-13 11:28:34 -0600

    hice el readme e hice la función de abrir imagen

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Ecualizaci-n-de-Histograma
 Proyecto Visión de Ecualización de Histograma
+## ¿Qué es una ecualización de histograma? 
+### es la transformación de una imagen para obtener un histograma con una distribución uniforme osea 
+### exista el mismo número de píxeles para cada nivel de gris del histograma de una imagen monocroma
+### Se define con ña función dada por: 
+### $$ v(i,j)= [Fu(u(i,j)-Fu(a))]/[1-Fu(a)][l-1]+0.5 $$
+### El resultado de una ecualización ebería maximizar el contraste de una imagen sin perder información de tipo estructural. 
+### En conclusión, nos sirven para: conviertir una imagen en un vector, 
+### que luego podemos combinar o aprovechar en aplicaciones como detección de rostros, clasificación de imágenes y localización de objetos.
--- ventana.py
+++ ventana.py
@@ -1,19 +1,38 @@
 # Importar bibliotecas
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+
 import sys
+
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtGui import QPixmap
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog
+
+
 # Instancia de la clase QApplication
 
 class ventana(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.hola = None
+        self.ruta = None
+        self.image = None
         self.inicializar()
+
     def inicializar(self):
-        self.hola=QPushButton('hola')
+        self.hola = QPushButton('hola')
         self.setCentralWidget(self.hola)
         self.show()
+
+    def hola(self):
+        img_name = QFileDialog.getOpenFileName(self, 'Abrir archivo', '/home/', '*.tif *.jpg *.png *.jpeg')
+        self.ruta = img_name[0]
+        orig_pix_map = QPixmap(self.ruta)
+        scaled_pix_map = orig_pix_map.scaled(QSize(400, 400), Qt.AspectRatioMode.KeepAspectRatio)
+        self.image.setPixmap(scaled_pix_map)
+
+
 # Iniciar el ciclo de eventos
 
-if __name__=="__main__":
-    app= QApplication(sys.argv)
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
     window = ventana()
     sys.exit(app.exec())
